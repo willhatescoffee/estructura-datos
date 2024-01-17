96fa5768fda7d4af3bf24844e2687fb65a10d6b8
willhatescoffee <willmar.huerta@unimilenium.edu.mx>
2024-01-17 11:33:42 -0600

    17/01/2024

--- estructuras_ejemplos.py
+++ estructuras_ejemplos.py
@@ -11,4 +11,9 @@ cadenaCaracter = cadena[0:3]
 #print(cadenaCaracter)
 
 ultimoCaracter = cadena[-1]
-print(ultimoCaracter)
+#print(ultimoCaracter)
+
+#CURP A G F J 99 01 26 (EJEMPLO) :)
+nombre = "Willmar Orlando Parroquin Huerta"
+
+
