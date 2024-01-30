4920eb0b6544fe7922aa5f9eaf71e1f1472977f4
willhatescoffee <willmar.huerta@unimilenium.edu.mx>
2024-01-29 19:06:01 -0600

    potencia_de_un_numero

--- Recursividad/tarearecursividad.py
+++ Recursividad/tarearecursividad.py
@@ -1,12 +1,11 @@
 #suma de elementos de un array
 
-def suma_array_recursiva(array, indice=0):
-    if indice == len(array):
+def suma_array(arr):
+    if not arr:
         return 0
     else:
-        return array[indice] + suma_array_recursiva(array, indice + 1)
-
-#Ejemplo de uso 
-mi_array = [1, 2, 3, 4, 5]
-resultado = suma_array_recursiva(mi_array)
-print(resultado)
+        return arr[0] + suma_array(1,2,3,4)
+    
+    resultado = suma_array
+    print("SUMA ARRAYS")
+    print(resultado)
