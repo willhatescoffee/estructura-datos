1c4c277009b6a920c1cb14c43b7dae5e7f246050
willhatescoffee <willmar.huerta@unimilenium.edu.mx>
2024-01-31 11:08:07 -0600

    lol

--- Recursividad/potenciadeunnumero.py
+++
@@ -1,9 +0,0 @@
-def calcular_potencia(base, exponente):
-    resultado = base ** exponente
-    return resultado
-
-# Ejemplo de uso
-base = float(input("Ingrese la base: "))
-exponente = int(input("Ingrese el exponente: "))
-resultado = calcular_potencia(base, exponente)
-print(f"{base} elevado a la {exponente} es igual a {resultado}")
--- memoria/quicksort.py
+++
@@ -1,8 +0,0 @@
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    privot = arr[len(arr)// 2]
-    left = [x for x in arr if x < privot]
-    middle = [x for x in arr if x == privot]
-    right = [x for x in arr if x > privot]
-    return quicksort(left) + middle + quicksort(right)
